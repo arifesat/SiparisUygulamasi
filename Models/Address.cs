1b6a2cb318fb7e4d59a71dbf4fe16b9e0d7961fa
Arif Esat Yilmaz <arif_esat@outlook.com>
2024-08-21 14:16:52 +0300

    added address model

---
+++ Models/Address.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace SiparisUygulamasi.Models
+{
+    public class Address
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Street")]
+        public string Street { get; set; }
+
+        [BsonElement("BuildingNo")]
+        public string BuildingNo { get; set; }
+
+        [BsonElement("DoorNo")]
+        public string DoorNo { get; set; }
+
+        [BsonElement("City")]
+        public string City { get; set; }
+
+        [BsonElement("PostalCode")]
+        public string PostalCode { get; set; }
+
+        [BsonElement("Country")]
+        public string Country { get; set; }
+
+    }
+}
