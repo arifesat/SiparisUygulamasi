4b6d117f1509790293540a9f6ac79277f5fad788
Arif Esat Yilmaz <arif_esat@outlook.com>
2024-08-26 11:31:30 +0300

    added token packages and login dto

---
+++ Dtos/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SiparisUygulamasi.Dtos
+{
+    public class LoginDTO
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
